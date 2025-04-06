90ad9e1318e681d3fb463a9c63ca19a0f257c791
Clararaaa <clara01angelica@gmail.com>
2025-04-06 20:38:55 +0700

    Menambahkan navigasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.foundation.layout)
+    implementation(libs.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
